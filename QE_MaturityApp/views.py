d9d9a4a87faf7ea815bae255475d69c566d3f97b
Morethanatester <benpavey@yahoo.com>
2024-05-26 15:27:37 +0100

    addee templated structure and basic css

--- QE_MaturityApp/views.py
+++ QE_MaturityApp/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, QE Maturity App!")
+    return render(request, "home.html", {})
