4f2187d6f446b103e11f7426977ccc8d4e8e0f26
Morethanatester <benpavey@yahoo.com>
2024-08-13 15:05:19 +0100

    delete test

--- QE_MaturityApp/tests/test_view.py
+++ QE_MaturityApp/tests/test_view.py
@@ -112,24 +112,6 @@ class ViewTests(TestCase):
         self.assertEqual(time_saved, 0)
         self.assertEqual(maturity_level, 'No Maturity')
 
-    # Test if calculate_new_scores function returns the correct values for high maturity
-    def test_calculate_new_scores_high_maturity(self):
-        request = self.client.request().wsgi_request
-        request.session['total_cost'] = 10000
-        request.session['cost_per_test'] = 100
-        request.session['total_execution_time_days'] = 10
-        
-        # Form data representing high maturity (14 questions with a score of 5 each)
-        form_data = {f'question_{i}': 5 for i in range(1, 15)}  # This should total 70 (5*14), which falls into "High Maturity"
-        form_data['num_executions'] = 5
-        
-        new_total_cost, new_cost_per_test, cost_savings, time_saved, maturity_level = calculate_new_scores(request, form_data)
-        
-        self.assertEqual(new_total_cost, 4000)  # 40% of original cost
-        self.assertEqual(new_cost_per_test, 40)  # 40% of original cost per test
-        self.assertEqual(cost_savings, 30000)  # 6000 * 5
-        self.assertEqual(time_saved, 30)  # 10 * 0.6 * 5
-        self.assertEqual(maturity_level, 'High Maturity')
 
     # Non-functional Tests
 
